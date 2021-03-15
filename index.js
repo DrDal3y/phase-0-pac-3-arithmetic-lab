aa5657deecc7ecb1bbd2c718226543f48b9c5010
Marcus <marcus.daley1@outlook.com>
2021-03-14 23:17:29 -0400

    Completed assignment

---
+++ index.js
@@ -0,0 +1,47 @@
+function add(x,y) {
+    return 1 + 8
+}
+function subtract(x,y) {
+    return 5 - 4
+}
+function multiply(x,y) {
+    return 4 * 7
+}
+function divide(x,y) {
+    return 25 / 5
+}
+function add(a, b){
+    return a + b
+}
+function subtract(a, b) {
+    return a - b
+}
+function multiply(a, b) {
+    return a * b
+}
+function divide(a, b) {
+    return a / b
+}
+function increment(a) {
+    return a + 1 
+}
+function decrement(a) {
+    return a - 1
+}
+function makeInt(n) {
+    parseInt(n);
+    return parseInt;
+}
+function preserveDecimal(n) {
+    parseFloat(n);
+    return parseFloat;
+}
+function makeInt(n) {
+    parseInt(n, 10);
+    return parseFloat(n);
+ 
+}
+function preserveDecimal(n) {
+    parseFloat(n);
+    return parseFloat(n);
+}
